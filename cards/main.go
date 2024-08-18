116fa0b1f5f2f8ae645006086227119c7726c53a
sebastian velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-08-18 17:32:24 -0500

    finished video 30

--- cards/main.go
+++ cards/main.go
@@ -3,10 +3,8 @@ package main
 // import "fmt"
 
 func main() {
-	// cards := newDeck()
-	// hand, remainingDeck := deal(cards, 5)
-	// cards.saveToFile("my_cards")
-	var cards deck = newDeckFromFile("my_cards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
 
