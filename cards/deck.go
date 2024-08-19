995a23af064921a88b9c4da87089fd19c8a2a4d4
sebastian velasco <11669998+sebasvelasco353@users.noreply.github.com>
2024-08-18 19:34:05 -0500

    finished video 36

--- cards/deck.go
+++ cards/deck.go
@@ -5,6 +5,7 @@ import (
 	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // create a new type of 'deck' which is a slice of strings
@@ -53,8 +54,11 @@ func newDeckFromFile(fileName string) deck {
 }
 
 func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
 	for i := range d {
-		newPosition := rand.Intn(len(d) - 1)
+		newPosition := r.Intn(len(d) - 1)
 		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
